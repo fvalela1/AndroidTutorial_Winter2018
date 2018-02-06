0630a3c45e9443c996096c2f12c23d82990bd670
Francesco Valela <valela.francesco@gmail.com>
2018-02-05 23:48:21 -0500

    need to fix one method in calculator

--- app/src/main/java/com/example/francescovalela/androidtutorialwinter2018/Calculator.kt
+++ app/src/main/java/com/example/francescovalela/androidtutorialwinter2018/Calculator.kt
@@ -7,24 +7,75 @@ class Calculator {
     val formula: Stack<FormulaItem> = Stack()
 
     fun pushOperand(operand: Double): Unit {
-        // TODO
+        if (formula.isEmpty())
+            return
+        else if (formula.peek().isOperand)
+            formula.pop()
+
+        formula.push(FormulaItem(operand))
     }
 
     fun pushOperator(operator: Char): Unit {
-        // TODO
+        if (formula.isEmpty())
+            return
+        else if (formula.peek().isOperand)
+            formula.pop()
+
+        formula.push(FormulaItem(operator = operator))
     }
 
     fun recalculate(): Double {
-        // TODO
-        return 0.0
+        if (formula.empty())
+            return 0.0
+        else if (formula.peek().isOperand)
+            formula.pop()
+
+        var total = 0.0
+        var mostRecentOperator: Char? = null
+
+        //TODO fix this up
+        for (item in formula) {
+            if (mostRecentOperator == null)
+                total += item.operand!!
+            else if (!item.isOperand)
+                mostRecentOperator = item.operator
+            else {
+                when (mostRecentOperator) {
+                    '+' -> addition(total, item.operand!!)
+                    'x' -> multiply(total, item.operand!!)
+                    '/' -> divide(total, item.operand!!)
+                    '-' -> subtract(total, item.operand!!)
+                    '√' -> return 0.0 //TODO does not work
+                    '%' -> mod(total, item.operand!!)
+                }
+                mostRecentOperator = null
+            }
+        }
+        return total
     }
 
+
     fun undo(): Unit {
-        // TODO
+        if (formula.peek().isOperand)
+            formula.pop()
+        formula.pop()
     }
 
     fun clear(): Unit {
         formula.clear()
     }
 
+    companion object Calculator {
+        fun addition(num1: Double = 0.0, num2: Double): Double = num1 + num2
+
+        fun subtract(num1: Double = 0.0, num2: Double): Double = num1 - num2
+
+        fun multiply(num1: Double = 0.0, num2: Double): Double = num1 * num2
+
+        fun divide(num1: Double = 0.0, num2: Double): Double = num1 / num2
+
+        fun mod(num1: Double = 0.0, num2: Double): Double = num1 % num2
+
+        fun sqrt(num1: Double = 0.0): Double = num1 * (1/num1)
+    }
 }
--- app/src/main/java/com/example/francescovalela/androidtutorialwinter2018/FormulaItem.kt
+++ app/src/main/java/com/example/francescovalela/androidtutorialwinter2018/FormulaItem.kt
@@ -1,13 +1,3 @@
 package com.example.francescovalela.androidtutorialwinter2018
 
-class FormulaItem(inOperand: Double?, inOperator: Char? = null) {
-
-    var isOperand: Boolean
-    val operand: Double = inOperand ?: 0.0
-    val operator: Char = inOperator ?: ' '
-
-    init {
-        isOperand = inOperator == null
-    }
-
-}
+data class FormulaItem(val operand: Double? = 0.0, val operator: Char? = ' ', val isOperand: Boolean = operator == ' ')
