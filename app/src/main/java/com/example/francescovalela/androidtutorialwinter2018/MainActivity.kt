b61e0faa41b0b12be06ff5deb9971fcb0ecbfe54
Jacob Gagne <jacob_gagne@hotmail.com>
2018-02-07 02:21:41 -0500

    simplified code for main activity and added automatic removal of extra 0 decimal

--- app/src/main/java/com/example/francescovalela/androidtutorialwinter2018/MainActivity.kt
+++ app/src/main/java/com/example/francescovalela/androidtutorialwinter2018/MainActivity.kt
@@ -7,10 +7,10 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    val calc = Calculator()
-    val numstack: Stack<Double> = Stack()
-    var lastOperator: Char? = null
-    var last: Double? = null
+    private val calc = Calculator()
+    private val digitStack: Stack<Double> = Stack()
+    private var lastOperator: Char? = null
+    private var last: Double? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +19,102 @@ class MainActivity : AppCompatActivity() {
         initializeListeners()
     }
 
-    fun initializeListeners(){
+    fun initializeListeners() {
+        setOperatorListeners()
+        setDigitListeners()
+
+        btn_clear.setOnClickListener {
+            calc.clear()
+            digitStack.clear()
+            last = null
+            lastOperator = null
+            refreshFormulaView()
+            refreshResultView(calc.recalculate())
+        }
+
+        btn_equals.setOnClickListener{
+            equalsListener()
+        }
+    }
+
+    private fun equalsListener() {
+        if(!digitStack.empty()) {
+            calc.pushOperand(getDigitValueFromStack())
+            digitStack.clear()
+            refreshResultView(calc.recalculate())
+            last = calc.recalculate()
+            lastOperator = null
+            refreshFormulaView()
+        }
+    }
+
+    private fun pushOperator(op: Char) {
+        if (!digitStack.empty()) {
+            val completeDigitValue = getDigitValueFromStack()
+            last = completeDigitValue
+            lastOperator = op
+            calc.pushOperand(completeDigitValue)
+            calc.pushOperator(op)
+            digitStack.clear()
+        } else if (last != null) {
+            calc.pushOperator(op)
+        }
+        lastOperator = op
+        refreshFormulaView()
+    }
+
+    private fun getDigitValueFromStack(): Double{
+        var ret = 0.0
+        digitStack.reversed().forEachIndexed { i, d ->
+            ret += d * Math.pow(10.0, (i).toDouble())
+        }
+        return ret
+    }
+
+    private fun refreshFormulaView() {
+        val completeDigitalValue = getDigitValueFromStack()
+        val curr: String = if (digitStack.empty()) "" else {
+            if (completeDigitalValue - getDigitValueFromStack().toLong() == 0.0)
+                completeDigitalValue.toLong().toString() else completeDigitalValue.toString()
+        }
+
+        when {
+            lastOperator != null && last != null -> {
+                val template = resources.getString(R.string.formula)
+                val tmp: Double = last ?: 0.0
+                val previous = if (tmp - tmp.toLong() == 0.0)
+                    tmp.toLong().toString() else tmp.toString()
+                formula.text = String.format(template, previous, lastOperator, curr)
+            }
+            last != null -> formula.text = last.toString()
+            else -> formula.text = curr
+        }
+    }
+
+    private fun refreshResultView(value: Double) {
+        result.text = if (value - value.toLong() == 0.0)
+            value.toLong().toString() else value.toString()
+    }
+
+    private fun genericDigitListener(digit: Double){
+        digitStack.push(digit);
+        refreshFormulaView()
+    }
+
+    private fun setDigitListeners() {
+        btn_0.setOnClickListener{ genericDigitListener(0.0) }
+        btn_1.setOnClickListener{ genericDigitListener(1.0) }
+        btn_2.setOnClickListener{ genericDigitListener(2.0) }
+        btn_3.setOnClickListener{ genericDigitListener(3.0) }
+        btn_4.setOnClickListener{ genericDigitListener(4.0) }
+        btn_5.setOnClickListener{ genericDigitListener(5.0) }
+        btn_6.setOnClickListener{ genericDigitListener(6.0) }
+        btn_7.setOnClickListener{ genericDigitListener(7.0) }
+        btn_8.setOnClickListener{ genericDigitListener(8.0) }
+        btn_9.setOnClickListener{ genericDigitListener(9.0) }
+    }
+
+    private fun setOperatorListeners() {
         btn_plus.setOnClickListener{ pushOperator('+'); }
         btn_minus.setOnClickListener{ pushOperator('-'); }
         btn_root.setOnClickListener{ pushOperator('√'); }
@@ -27,80 +122,5 @@ class MainActivity : AppCompatActivity() {
         btn_multiply.setOnClickListener{ pushOperator('*'); }
         btn_divide.setOnClickListener{ pushOperator('/'); }
         btn_modulo.setOnClickListener{ pushOperator('%'); }
-
-        btn_0.setOnClickListener{ numstack.push(0.0); refreshNumView() }
-        btn_1.setOnClickListener{ numstack.push(1.0); refreshNumView() }
-        btn_2.setOnClickListener{ numstack.push(2.0); refreshNumView() }
-        btn_3.setOnClickListener{ numstack.push(3.0); refreshNumView() }
-        btn_4.setOnClickListener{ numstack.push(4.0); refreshNumView() }
-        btn_5.setOnClickListener{ numstack.push(5.0); refreshNumView() }
-        btn_6.setOnClickListener{ numstack.push(6.0); refreshNumView() }
-        btn_7.setOnClickListener{ numstack.push(7.0); refreshNumView() }
-        btn_8.setOnClickListener{ numstack.push(8.0); refreshNumView() }
-        btn_9.setOnClickListener{ numstack.push(9.0); refreshNumView() }
-
-        btn_clear.setOnClickListener{
-            calc.clear()
-            refreshResult(calc.recalculate())
-        }
-
-        btn_equals.setOnClickListener{
-            equals()
-        }
-    }
-
-    fun equals(){
-        if(!numstack.empty()) {
-            calc.pushOperand(getNumstackValue())
-            numstack.clear()
-            refreshResult(calc.recalculate())
-            last = calc.recalculate()
-            lastOperator = null
-            refreshNumView()
-        }
-    }
-
-    fun pushOperator(op: Char){
-        if(lastOperator != null){
-            equals()
-            lastOperator = op
-            refreshNumView()
-        }
-        if(!numstack.empty()) {
-            val numstackVal = getNumstackValue()
-            last = numstackVal
-            lastOperator = op
-            calc.pushOperand(numstackVal)
-            numstack.clear()
-            calc.pushOperator(op)
-            refreshNumView()
-        }
-        else if(last != null){
-            lastOperator = op
-            calc.pushOperator(op)
-            refreshNumView()
-        }
-    }
-
-    fun getNumstackValue(): Double{
-        var i = 1
-        var ret = 0.0
-        numstack.reversed().forEach {
-            ret += it * i
-            i *= 10
-        }
-
-        return ret
-    }
-
-    fun refreshNumView() {
-        val curr = if (numstack.empty()) "" else getNumstackValue().toString()
-        if(lastOperator != null) formula.text = """${last.toString()} $lastOperator $curr"""
-        else if (last != null )formula.text = last.toString()
-        else formula.text = curr
-    }
-
-    fun refreshResult(value: Double) {
-        result.text = value.toString()
     }
 }
